394739d2e7952cc7113380f49ac62db01359e405
Nikhil Pandey <np8654750@gmail.com>
2025-09-26 01:10:25 +0530

    Update Wrapper.jsx

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,9 +1,23 @@
 // Wrapper.jsx
-import React from "react";
+// import React from "react";
+
+// const Wrapper = ({ children }) => {
+//   return (
+//     <div className="min-w-[1250px]  mx-auto flex flex-col items-center gap-10">
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default Wrapper;
+
+
+
+import React from 'react';
 
 const Wrapper = ({ children }) => {
   return (
-    <div className="min-w-[1250px]  mx-auto flex flex-col items-center gap-10">
+    <div className="w-full max-w-[1250px] mx-auto flex flex-col items-center gap-10 lg:p-5">
       {children}
     </div>
   );
